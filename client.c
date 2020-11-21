56016d77c72c10393771308b59aac013bbf228bb
dohun <yh08037@gmail.com>
2020-11-21 17:49:24 +0900

    added options (ipv4_address, port_number)

--- client.c
+++ client.c
@@ -10,19 +10,52 @@
 
 #define SIZE_BUFFER 256
 
-int main()
-{
-	int sockfd, len, result;
-	struct sockaddr_in address;
-	char msg[SIZE_BUFFER];
-	
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = inet_addr("127.0.0.1");
-	address.sin_port = htons(9734);
+static void usage(void) {
+    fprintf(stderr, "\n"
+		"usage: client [-h] [options]"
+		"\n"
+		"options:\n"
+		"    -h  show this message\n"
+		"    -d  ipv4 address of server (default: 127.0.0.1)\n"
+		"    -p  port number of server (default: 9734)\n"
+		"\n"
+	);
+	exit(1);
+}
 
-	len = sizeof(address);
+int main(int argc, char* argv[])
+{
+	int                     sockfd, opt, len, result;
+	struct sockaddr_in      address;
+	char                    msg[SIZE_BUFFER];
+
+	char*					ipv4_address = "127.0.0.1";
+	int						port_number = 9734;
+	
+
+    while ((opt = getopt(argc, argv, "hd:p:")) != -1) {
+        switch (opt) {
+        case 'h':
+            usage();
+            break;
+        case 'd':
+            ipv4_address = strdup(optarg);
+			break;
+		case 'p':
+			port_number = atoi(strdup(optarg));
+			break;
+        default:
+            break;
+        }
+    }	
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    address.sin_family = AF_INET;
+	address.sin_addr.s_addr = inet_addr(ipv4_address);
+	address.sin_port = htons(port_number);
+
+    len = sizeof(address);
 	result = connect(sockfd, (struct sockaddr *)&address, len);
 
 	if (result == -1) {
